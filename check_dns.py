bb894c162c1333f185fbeb69ce9c2e6a505602b3
Xavid Pretzer <xavid@mit.edu>
2010-03-04 14:13:22 -0500

    * Better Wordpress detection.  * Add a more-likely-to-be-clickable URL to dns emails.

--- check_dns.py
+++ check_dns.py
@@ -20,17 +20,21 @@ def check_dns():
         if util.points_at_scripts(t.hostname):
             path = '/mit/%s/web_scripts/%s' % (t.locker,
                                                vhosts.get_path(t.locker,t.hostname))
+            wordpress = "This site looks like a WordPress blog; for the new URL to work properly, you'll need to access the WordPress admin interface via your old URL, go to General Settings, and change the WordPress address and Blog address to 'http://%s'." % hostname
             
             # Try to figure out what's up with the hostname currently
             try:
                 page = urllib2.urlopen('http://%s/'%t.hostname)
                 content = page.read()
-                if '<meta name="generator" content="WordPress' in content:
-                    sitestatus = "This site looks like a WordPress blog; for the new URL to work properly, you'll need to access the WordPress admin interface via your old URL, go to General Settings, and change the WordPress address and Blog address to 'http://%s'." % hostname
+                if ('<meta name="generator" content="WordPress' in content
+                    or 'wp-login' in page.geturl()):
+                    sitestatus = wordpress
                 else:
-                    sitestatus = "Your site appears to be working properly."
+                    sitestatus = "Your site appears to be working properly.  Have fun"
             except urllib2.HTTPError,e:
-                if e.code == 404:
+                if 'wp-login' in e.geturl():
+                    sitestatus = wordpress
+                elif e.code == 404:
                     sitestatus = "There doesn't seem to be any content currently at %s; make sure that directory exists and has an index.html, index.cgi, or similar, or change this hostname to point somewhere else at http://pony.scripts.mit.edu." % path
                 elif e.code == 403:
                     sitestatus = "Visiting that page yields a Forbidden error; this is often caused by a lack of valid content at %s.  Putting an index.html, index.cgi, or similar there may solve this." % path
@@ -40,7 +44,7 @@ def check_dns():
             subject = u"Hostname %s now working"%t.hostname
             body = u"""Hello,
 
-Just wanted to let you know that the hostname %(hostname)s is now configured and working.  It currently points to %(path)s.
+Just wanted to let you know that the hostname %(hostname)s is now configured and working.  It currently points to %(path)s.  Visit http://%(hostname)s/ to check it out.
 
 %(sitestatus)s
 
