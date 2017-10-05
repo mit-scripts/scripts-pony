490aeb4244ade1ecb7151b92af65193cdfcc2ac3
Anders Kaseorg <andersk@mit.edu>
2017-10-05 08:55:59 -0400

    Automatically install certificates received from mitcert
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

---
+++ handle_cert_mail.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import sys
+import ldap
+import ldap.filter
+from scripts import cert, log
+from scriptspony import vhosts
+
+BLACKLIST = ['scripts.mit.edu', '*.scripts.mit.edu']
+
+@log.exceptions
+def main():
+    msg = sys.stdin.read()
+    pem = cert.msg_to_pem(msg)
+    if pem is None:
+        log.info('handle_cert_mail.py: No certificate')
+        return
+    chain = cert.pem_to_chain(pem)
+    names = cert.chain_subject_names(chain)
+
+    filter = ldap.filter.filter_format(
+        '(&(objectClass=scriptsVhost)(|' +
+        ''.join('(scriptsVhostName=%s)' for name in names) +
+        ''.join('(scriptsVhostAlias=%s)' for name in names) +
+        ')' +
+        ''.join('(!(scriptsVhostName=%s))' for name in BLACKLIST) +
+        ')',
+        list(names) +
+        list(names) +
+        BLACKLIST)
+
+    vhosts.connect()
+    res = vhosts.conn.search_s(
+        'ou=VirtualHosts,dc=scripts,dc=mit,dc=edu',
+        ldap.SCOPE_ONELEVEL,
+        filter,
+        ['scriptsVhostName', 'scriptsVhostAccount', 'scriptsVhostCertificate'])
+    if res:
+        for dn, attrs in res:
+            vhost, = attrs['scriptsVhostName']
+            account, = attrs['scriptsVhostAccount']
+            if 'scriptsVhostCertificate' in attrs:
+                old_scripts, = attrs['scriptsVhostCertificate']
+                old_chain = cert.scripts_to_chain(old_scripts)
+            else:
+                old_chain = None
+            if cert.chain_should_install(chain, old_chain):
+                log.info('handle_cert_mail.py: Installing certificate for %s on %s' % (vhost, account))
+                vhosts.conn.modify_s(
+                    dn,
+                    [(ldap.MOD_REPLACE,
+                      'scriptsVhostCertificate',
+                      cert.chain_to_scripts(chain)),
+                     (ldap.MOD_REPLACE,
+                      'scriptsVhostCertificateKeyFile',
+                      'scripts-2048.key')])
+            else:
+                log.info('handle_cert_mail.py: IGNORING certificate for %s on %s' % (vhost, account))
+    else:
+        log.error('handle_cert_mail.py: Certificate for %s matches no vhost' % list(names))
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/cert.py
@@ -0,0 +1,90 @@
+import base64
+from datetime import datetime
+import re
+from OpenSSL import crypto
+import pyasn1.codec.der.decoder as der_decoder
+from pyasn1_modules.rfc2459 import SubjectAltName
+import requests
+
+SCRIPTS_PUBKEY = base64.b64decode('''\
+MIIBDQIBAAKCAQEAxJo8HD63ei1mpkNGQVv3wXXGrQUiDON1FhsJ6iYiCFmxmOKNvFssPlQ48uIB
+JBNCqLh8EkmnmecSI5kDPlDGy/rq2lZC7OrSfcsNzTP9fXHi5kvYoOS6XuVuLf/yDglp7T/7qcrM
+PXX4KBDcaILnEH9Y8Leg8UBVf2xGgSF+Pe62oTR7BX8+g9TUUp6pdycdwr6JCwJaRKnokys2CksY
+yOlVdOZByV0ZVHZjVC4uOwn72GfLJEdni7wYZ76tgWXW2c1l3j09wL47BfBtDq3W9STke5HS2SRv
+Wh/U2tDLIzO+mzBR1mrkk+gs8XGC919jFXQzBqDNrmUmrtT6YrSAHwIDAQAB
+''')
+
+def pem_to_chain(data):
+    certs = [
+        crypto.load_certificate(crypto.FILETYPE_PEM, m.group(0))
+        for m in
+        re.finditer(
+            b'-----BEGIN CERTIFICATE-----\r?\n.+?\r?\n-----END CERTIFICATE-----',
+            data, re.DOTALL)
+    ]
+
+    # Put the chain in the right order, and delete any self-signed root
+    leaf, = [c for c in certs if not any(
+        c1.get_issuer() == c.get_subject() for c1 in certs)]
+    chain = [leaf]
+    count = 1
+    while True:
+        issuers = [c for c in certs if chain[-1].get_issuer() == c.get_subject()]
+        if not issuers:
+            break
+        issuer, = issuers
+        assert issuer not in chain
+        count += 1
+        if issuer.get_issuer() == issuer.get_subject():
+            break
+        chain.append(issuer)
+    assert count == len(certs)
+
+    return chain
+
+def chain_to_scripts(chain):
+    return b' '.join(base64.b64encode(
+        crypto.dump_certificate(crypto.FILETYPE_ASN1, c)) for c in chain)
+
+def scripts_to_chain(data):
+    return [
+        crypto.load_certificate(crypto.FILETYPE_ASN1, base64.b64decode(d))
+        for d in data.split(b' ')
+    ]
+
+def chain_to_pem(chain):
+    return b''.join(crypto.dump_certificate(crypto.FILETYPE_PEM, c) for c in chain)
+
+def chain_subject_names(chain):
+    names = {name for field, name in chain[0].get_subject().get_components() if field == 'CN'}
+    for i in range(chain[0].get_extension_count()):
+        ext = chain[0].get_extension(i)
+        if ext.get_short_name() == 'subjectAltName':
+            value, substrate = der_decoder.decode(ext.get_data(), asn1Spec=SubjectAltName())
+            assert substrate == b''
+            for component in value:
+                if component.getName() == 'dNSName':
+                    names.add(str(component.getComponent()))
+    return names
+
+def chain_notAfter(chain):
+    return min(datetime.strptime(c.get_notAfter(), '%Y%m%d%H%M%SZ') for c in chain)
+
+def chain_should_install(new_chain, old_chain=None):
+    if crypto.dump_privatekey(crypto.FILETYPE_ASN1, new_chain[0].get_pubkey()) != SCRIPTS_PUBKEY:
+        return False
+
+    if old_chain and not chain_notAfter(new_chain) > chain_notAfter(old_chain):
+        return False
+
+    if old_chain and not chain_subject_names(new_chain).issuperset(chain_subject_names(old_chain)):
+        return False
+
+    # TODO actually verify the chain, once we have PyOpenSSL 0.15
+
+    return True
+
+def msg_to_pem(msg):
+    url, = set(re.findall(r'https://cert-manager\.com/customer/InCommon/ssl\?action=download&sslId=\d+&format=x509', msg))
+    r = requests.get(url)
+    return r.text if r.status_code == 200 else None
