5e85873fec27013fb55701583edeed5a8a6c522e
Quentin Smith <quentin@mit.edu>
2020-03-06 02:14:03 -0500

    Allow configuring the path and principal for keytabs

--- scripts/keytab.py
+++ scripts/keytab.py
@@ -3,19 +3,23 @@ import subprocess
 import threading
 from datetime import datetime, timedelta
 
+from tg import config
+
 KEYTAB_FILE = None
 principal = None
 
 
-def set(name):
-    """Tells the keytab module to look for the daemon/NAME.mit.edu
-    keytab in ~/Private/NAME.keytab"""
+def set(keytab_file, k5principal):
+    """Tells the keytab module to look for the k5principal
+    keytab in keytab_file"""
     global KEYTAB_FILE, principal
-    KEYTAB_FILE = os.path.expanduser("~/Private/%s.keytab" % name)
-    principal = "daemon/%s.mit.edu" % name
+    KEYTAB_FILE = os.path.expanduser(keytab_file)
+    principal = k5principal
 
 
 def exists():
+    if not KEYTAB_FILE:
+        set(config.get('keytab'), config.get('principal'))
     return os.path.exists(KEYTAB_FILE)
 
 
